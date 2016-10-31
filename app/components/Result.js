3ba9b57f8e1b890f473ffc57caac81bcd59f13ac
Alex Pilewski <pilewski.alex@gmail.com>
2016-10-31 15:20:27 -0600

    create result component

---
+++ app/components/Result.js
@@ -0,0 +1,13 @@
+const React = require('react');
+
+class Result extends React.Component {
+  render() {
+    return(
+      <section className='result'>
+        THIS IS WHERE THE RESULTS ARE GONNA GO
+      </section>
+    )
+  }
+};
+
+module.exports = Result;
